7ee5ce7ddbcb2547394e9343bfec19b3323226e8
Visulth <lordvisulth@gmail.com>
2025-12-14 02:59:05 -0500

    Removed incorrect namespace

--- BuildPlatformsAnywhere/Patch.cs
+++ BuildPlatformsAnywhere/Patch.cs
@@ -5,57 +5,55 @@ using System;
 using System.Linq;
 using System.Reflection;
 
-namespace NoSupports
+
+[HarmonyPatch(typeof(BuildingManager))]
+class Patch
 {
-	[HarmonyPatch(typeof(BuildingManager))]
-	class Patch
-    {
-		//static AccessTools.FieldRef<PauseMenu, TMP_Text> TotalOrePiecesTextRef = AccessTools.FieldRefAccess<PauseMenu, TMP_Text>("TotalOrePiecesText");
-		//static void Postfix(PauseMenu __instance)
+	//static AccessTools.FieldRef<PauseMenu, TMP_Text> TotalOrePiecesTextRef = AccessTools.FieldRefAccess<PauseMenu, TMP_Text>("TotalOrePiecesText");
+	//static void Postfix(PauseMenu __instance)
 
-		//public CanPlaceBuilding CanPlaceObject(Vector3Int position, BuildingObject objPrefab, Quaternion rotation, bool requiresFlatGround, PlacementNodeRequirement placementNodeRequirement, out BuildingPlacementNode AttachedNode, ToolBuilder activeTool)
+	//public CanPlaceBuilding CanPlaceObject(Vector3Int position, BuildingObject objPrefab, Quaternion rotation, bool requiresFlatGround, PlacementNodeRequirement placementNodeRequirement, out BuildingPlacementNode AttachedNode, ToolBuilder activeTool)
 
-		static MethodBase TargetMethod()
-		{
-			return AccessTools.Method(
-				typeof(BuildingManager),
-				"CanPlaceObject",
-				new[]
-				{
-					typeof(Vector3Int),
-					typeof(BuildingObject),
-					typeof(Quaternion),
-					typeof(bool),
-					typeof(PlacementNodeRequirement),
-					typeof(BuildingPlacementNode).MakeByRefType(), // out
-					typeof(ToolBuilder)
-				}
-			);
-		}
-
-		[HarmonyPrefix]
-		static bool CanPlaceBuilding_Prefix(BuildingManager __instance, ref CanPlaceBuilding __result)
-		{
-			var objectID = __instance.GetGhostObject().GetSavableObjectID();
-
-			if (!ShouldAllowObject(objectID))
-				return true;
-			
-			__result = CanPlaceBuilding.Valid;
-
-			return false;
-		}
-
-		static bool ShouldAllowObject(SavableObjectID objectID)
-		{
-			switch (objectID)
+	static MethodBase TargetMethod()
+	{
+		return AccessTools.Method(
+			typeof(BuildingManager),
+			"CanPlaceObject",
+			new[]
 			{
-				case SavableObjectID.Scaffold2x2:
-				case SavableObjectID.Scaffold2x2_Ramp:
-					return true;
+				typeof(Vector3Int),
+				typeof(BuildingObject),
+				typeof(Quaternion),
+				typeof(bool),
+				typeof(PlacementNodeRequirement),
+				typeof(BuildingPlacementNode).MakeByRefType(), // out
+				typeof(ToolBuilder)
 			}
-			return false;
-		}
-
+		);
 	}
+
+	[HarmonyPrefix]
+	static bool CanPlaceBuilding_Prefix(BuildingManager __instance, ref CanPlaceBuilding __result)
+	{
+		var objectID = __instance.GetGhostObject().GetSavableObjectID();
+
+		if (!ShouldAllowObject(objectID))
+			return true;
+			
+		__result = CanPlaceBuilding.Valid;
+
+		return false;
+	}
+
+	static bool ShouldAllowObject(SavableObjectID objectID)
+	{
+		switch (objectID)
+		{
+			case SavableObjectID.Scaffold2x2:
+			case SavableObjectID.Scaffold2x2_Ramp:
+				return true;
+		}
+		return false;
+	}
+
 }
